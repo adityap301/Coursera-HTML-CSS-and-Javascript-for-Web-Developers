cee390af95383c28576d9a49212b4cb6d775a993
Aditya Parthasarathy <adityap301@gmail.com>
2020-09-18 10:31:55 +0530

    Step 1 done

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+( function() {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -63,3 +63,5 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
     // helloSpeaker.xxxx
   }
 }
+
+})();
